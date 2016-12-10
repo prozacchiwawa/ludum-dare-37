472d2a264d4aadf1e7c50a577e7f0967e6c720ba
prozacchiwawa <art.yerkes@gmail.com>
2016-12-09 22:51:01 -0800

    Moving elevator

--- src/Main.kt
+++ src/Main.kt
@@ -175,9 +175,6 @@ class Floor(number : Int) : InScene {
 }
 
 class Hero : InScene {
-    var x = 0.0
-    var y = 0.0
-    var z = 0.0
     val box = newBoxGeometry(0.3, 1.3, 0.4)
     val material = newMeshLambertMaterial(0x8caadb)
     val mesh = newMesh(box, material)
@@ -196,6 +193,70 @@ class Hero : InScene {
 
     override fun removeFromScene(scene: Scene) {
         scene.remove(group)
+    }
+}
+
+class Elevator(min : Int, max : Int) : InScene {
+    var floor = min
+    var direction = false
+    var open = -1.0
+    val min = min
+    val max = max
+    val espeed = 1.0
+    val openTime = 2.0
+
+    val elevatorBackGeom = newBoxGeometry(1.0, 2.0, 0.1)
+    val elevatorBackMat = newMeshLambertMaterial(0x3f4f3e)
+    val elevatorBack = newMesh(elevatorBackGeom, elevatorBackMat)
+    val elevatorDoorGeom = newBoxGeometry(1.0, 2.0, 0.1)
+    val elevatorDoorMat = newMeshLambertMaterial(0x3f4f3e)
+    val elevatorDoor = newMesh(elevatorDoorGeom, elevatorDoorMat)
+    val elevatorFloorGeom = newBoxGeometry(1.0, 0.1, 2.0)
+    val elevatorFloorMat = newMeshLambertMaterial(0xdae0d9)
+    val elevatorFloor = newMesh(elevatorFloorGeom, elevatorFloorMat)
+    val group = newGroup()
+
+    init {
+        elevatorBack.o.position.y = 1.0
+        elevatorBack.o.position.z = -2.0
+        elevatorDoor.o.position.y = 1.0
+        elevatorDoor.o.position.z = 0.0
+        group.add(elevatorBack)
+        group.add(elevatorFloor)
+        group.o.position.y = 2.0
+    }
+
+    override fun addToScene(scene : Scene) {
+        scene.add(group)
+    }
+
+    override fun removeFromScene(scene: Scene) {
+        scene.remove(group)
+    }
+
+    fun update(time : Double) {
+        val targetY = floor * floorHeight
+        if (targetY != group.o.position.y) {
+            open = -1.0
+            if (group.o.position.y < targetY) {
+                group.o.position.y = Math.min(group.o.position.y + (espeed * time), targetY)
+            } else {
+                group.o.position.y = Math.max(group.o.position.y - (espeed * time), targetY)
+            }
+        } else {
+            if (open == -1.0) {
+                open = openTime
+                group.remove(elevatorDoor)
+            } else {
+                open = Math.max(open - time, 0.0)
+                if (open == 0.0) {
+                    group.add(elevatorDoor)
+                    floor = if (direction) { Math.min(floor + 1, max) } else { Math.max(floor - 1, min) }
+                    if (floor == max) { direction = false }
+                    if (floor == min) { direction = true }
+                }
+            }
+        }
     }
 }
 
@@ -223,22 +284,26 @@ class GameContainer() {
     var targetFloor = 0
 
     val hero = Hero()
+    val elevator = Elevator(1, 3)
 
     fun addToScene(scene : Scene) {
         scene.add(light)
         hero.addToScene(scene)
+        elevator.addToScene(scene)
         floors.forEach { f -> f.addToScene(scene) }
     }
 
     fun removeFromScene(scene : Scene) {
         scene.remove(light)
         hero.removeFromScene(scene)
+        elevator.removeFromScene(scene)
         floors.forEach { f -> f.removeFromScene(scene) }
     }
 
     fun update(m : GameUpdateMessage) {
         if (m.tag == GameUpdateMessageTag.NewFrame) {
             curTime += m.time
+            elevator.update(m.time)
             cameraY = (cameraY + (targetCameraY * 3.0 * m.time)) / (1.0 + 3.0 * m.time)
             camera.o.position.set( 0, cameraY, 15.0 )
             camera.o.lookAt( 0, 1.0, 0.0 )
