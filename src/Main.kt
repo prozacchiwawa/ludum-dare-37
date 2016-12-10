bd4a80d3db15e1deb1b6f2759085fcc635bf1334
prozacchiwawa <art.yerkes@gmail.com>
2016-12-09 22:11:47 -0800

    Elevator shaft

--- src/Main.kt
+++ src/Main.kt
@@ -117,9 +117,8 @@ val floorHeight = 2.0
 
 class Floor(number : Int) {
     val number = number
-    val backWallGeom = newBoxGeometry(100.0, floorHeight, 0.2)
+    val backWallGeom = newBoxGeometry(20.0, floorHeight, 0.2)
     val backWallMaterial = newMeshLambertMaterial(0xf2e9c4)
-    val backWall = newMesh(backWallGeom, backWallMaterial)
     val floorGeom = newBoxGeometry(100.0, 0.2, 2.0)
     val floorMaterial = newMeshLambertMaterial(0x006600)
     val floor = newMesh(floorGeom, floorMaterial)
@@ -132,9 +131,17 @@ class Floor(number : Int) {
     val group = newGroup()
 
     init {
-        backWall.o.position.y = 1.0
+        val backWallLeft = newMesh(backWallGeom, backWallMaterial)
+        backWallLeft.o.position.x = -10.5
+        backWallLeft.o.position.y = floorHeight / 2.0
+        backWallLeft.o.position.z = 0.0
+        group.add(backWallLeft)
+        val backWallRight = newMesh(backWallGeom, backWallMaterial)
+        backWallRight.o.position.x = 10.5
+        backWallRight.o.position.y = floorHeight / 2.0
+        backWallRight.o.position.z = 0.0
+        group.add(backWallRight)
         floor.o.position.z = 1.0
-        group.add(backWall)
         group.add(floor)
         group.o.position.y = number * floorHeight
         doors = (0..10).map({ n ->
@@ -148,7 +155,8 @@ class Floor(number : Int) {
             handle.o.position.z = 0.15
             d.add(plate)
             d.add(handle)
-            d.o.position.x = (n - 6.0) * 4.0
+            val elevatorBump = if (n < 6) { 1.0 } else { 0.0 }
+            d.o.position.x = (n - 5.0 - elevatorBump) * 4.0
             d
         }).toMutableList()
         doors.forEach({ d -> group.add(d) })
